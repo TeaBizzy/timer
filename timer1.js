9a594b56dc586e3d8bd85a0ce0bdfb32bbd75d75
Stefan <satalbot@protonmail.com>
2022-10-25 18:25:25 +0000

    Creates timers for each argument in seconds, and plays a beep when the timer reachers 0

---
+++ timer1.js
@@ -0,0 +1,20 @@
+const args = process.argv.slice(2);
+
+console.log(args);
+
+const createTimers = function() {
+  if (args.length === 0) {
+    return;
+  }
+
+  for (const argument of args) {
+    let timer = Number(argument);
+    if (isNaN(timer) || timer <= 0) {
+      return;
+    }
+    timer *= 1000;
+    setTimeout(() => process.stdout.write("\007"), timer);
+  }
+};
+
+createTimers();
